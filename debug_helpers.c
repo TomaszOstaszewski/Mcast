0823d8bacbe771b2183394ad005b9363084cc983
Tomasz Ostaszewski <tomasz.ostaszewski@interia.pl>
2012-01-07 15:24:03 +0100

    Updated debug_helpers.{c,h}

--- debug_helpers.c
+++ debug_helpers.c
@@ -9,10 +9,29 @@
 #include "pcc.h"
 #include "debug_helpers.h"
 
+/*!
+ * @brief Output buffer size for debug_output and debug_outpuln.
+ */
 #define OUTPUT_BUFFER_LEN (256)
 
+/*!
+ * @brief Output buffer size for ErrorHandlerEx.
+ */
+#define BUFFER_SIZE (256)
+
+/*!
+ * @brief 
+ * @param[in] formatString
+ * @author T.Ostaszewski
+ */
 static __declspec(thread) TCHAR outputBuffer[OUTPUT_BUFFER_LEN];
 
+/*!
+ * @brief Helper function, actual implementation of both debug_output and debug_outputln
+ * @param[in] formatString format string for printf
+ * @param[in] args arguments list for printf
+ * @author T.Ostaszewski
+ */
 static HRESULT debug_output_args(LPCTSTR formatString, va_list args)
 {
 	HRESULT hr;
@@ -44,11 +63,47 @@ HRESULT debug_outputln(LPCTSTR formatString, ...)
 	return hr;
 }
 
+/*!
+ * @brief Frees the buffer allocated via get_text_hresult
+ * @param[in] psz_buffer - pointer to the buffer allocated via get_text_hresult
+ * @author T.Ostaszewski
+ * @sa get_text_hresult
+ */
+static void free_text_hresult(const char * psz_buffer)
+{
+    LocalFree( (LPVOID)psz_buffer );
+}
+
+/*!
+ * @brief Returns text for given HRESULT
+ * @param[in] hr HRESULT code for which text description is to be looked upon
+ * @return pointer to a buffer that contains textual description of the HRESULT given
+ * @sa free_text_hresult
+ * @author T.Ostaszewski
+ */
+static const char * get_text_hresult(HRESULT hr)
+{
+    LPVOID lpMsgBuf;
+    FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER |
+            FORMAT_MESSAGE_FROM_SYSTEM |
+            FORMAT_MESSAGE_IGNORE_INSERTS,
+            NULL,
+            GetLastError(),
+            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
+            (LPTSTR) &lpMsgBuf,
+            0,
+            NULL);
+    // Process any inserts in lpMsgBuf.
+    // ...
+    // Display the string.
+    // Free the buffer.
+    return (const char *)lpMsgBuf;
+}
+
 void ErrorHandlerEx( WORD wLine, LPSTR lpszFile )
 {
 	LPVOID lpvMessage;
 	DWORD  dwError;
-#define BUFFER_SIZE (256)
 	TCHAR  szBuffer[BUFFER_SIZE];
 
 	// Allow FormatMessage() to look up the error code returned by GetLastError
@@ -81,27 +136,6 @@ void ErrorHandlerEx( WORD wLine, LPSTR lpszFile )
 	MessageBox(NULL, lpvMessage, szBuffer, MB_ICONEXCLAMATION | MB_OK);
 }
 
-static const char * get_text_hresult(HRESULT hr)
-{
-    LPVOID lpMsgBuf;
-    FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER |
-            FORMAT_MESSAGE_FROM_SYSTEM |
-            FORMAT_MESSAGE_IGNORE_INSERTS,
-            NULL,
-            GetLastError(),
-            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
-            (LPTSTR) &lpMsgBuf,
-            0,
-            NULL);
-    // Process any inserts in lpMsgBuf.
-    // ...
-    // Display the string.
-    // Free the buffer.
-    return (const char *)lpMsgBuf;
-}
 
-static void free_text_hresult(const char * psz_buffer)
-{
-    LocalFree( (LPVOID)psz_buffer );
-}
+
 
--- debug_helpers.h
+++ debug_helpers.h
@@ -15,8 +15,34 @@ extern "C" {
 #endif 
 #include <windows.h>
 
+/*!
+ * @brief Outputs a formated text into the debug window 
+ * @details The debug window is the one to which we write using OutputDebugString. 
+ * Usually, it is attached to a debugger, but can also be seen using DebugView application
+ * from Sysinternal.
+ * @param[in] formatString
+ * @return
+ * @author T.Ostaszewski
+ */
 HRESULT debug_output(LPCTSTR formatString, ...);
+
+/*!
+ * @brief Outputs a formated text into the debug window, appends newline at the end.
+ * @details The debug window is the one to which we write using OutputDebugString. 
+ * Usually, it is attached to a debugger, but can also be seen using DebugView application
+ * from Sysinternal.
+ * @param[in] formatString
+ * @return
+ * @author T.Ostaszewski
+ */
 HRESULT debug_outputln(LPCTSTR formatString, ...);
+
+/*!
+ * @brief Displays a message box with a textual code for HRESULT given
+ * @param[in] wLine line number to be displayed in the message box
+ * @param[in] lpszFile file name to be displayed in the message box
+ * @author T.Ostaszewski
+ */
 void ErrorHandlerEx(WORD wLine, LPSTR lpszFile);
 
 #if defined __cplusplus
