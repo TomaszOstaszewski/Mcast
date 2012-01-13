a316c9cd6827e88b1047c770f79614e898ae6a29
Tomasz Ostaszewski <tomasz.ostaszewski@interia.pl>
2012-01-13 20:23:14 +0100

    Added copyright license.

--- debug_helpers.c
+++ debug_helpers.c
@@ -2,6 +2,26 @@
 /**
  * @file debug_helpers.c
  * @author T. Ostaszewski
+ * @par License
+ * @code Copyright 2012 Tomasz Ostaszewski. All rights reserved.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * 	1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *	2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation 
+ * 	and/or other materials provided with the distribution.
+  * THIS SOFTWARE IS PROVIDED BY Tomasz Ostaszewski AS IS AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ * IN NO EVENT SHALL Tomasz Ostaszewski OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+ * SUCH DAMAGE.
+  * The views and conclusions contained in the software and documentation are those of the 
+ * authors and should not be interpreted as representing official policies, 
+ * either expressed or implied, of Tomasz Ostaszewski.
+ * @endcode
  * @date 04-Jan-2012
  * @brief 
  * @details 
@@ -29,7 +49,6 @@ static __declspec(thread) TCHAR outputBuffer[OUTPUT_BUFFER_LEN];
  * @brief Helper function, actual implementation of both debug_output and debug_outputln
  * @param[in] formatString string for printf
  * @param[in] args arguments list for printf
- * @author T.Ostaszewski
  */
 static HRESULT debug_output_args(LPCTSTR formatString, va_list args)
 {
@@ -65,7 +84,6 @@ HRESULT debug_outputln(LPCTSTR formatString, ...)
 /*!
  * @brief Frees the buffer allocated via get_text_hresult
  * @param[in] psz_buffer - pointer to the buffer allocated via get_text_hresult
- * @author T.Ostaszewski
  * @sa get_text_hresult
  */
 static void free_text_hresult(const char * psz_buffer)
@@ -78,7 +96,6 @@ static void free_text_hresult(const char * psz_buffer)
  * @param[in] hr HRESULT code for which text description is to be looked upon
  * @return pointer to a buffer that contains textual description of the HRESULT given
  * @sa free_text_hresult
- * @author T.Ostaszewski
  */
 static const char * get_text_hresult(HRESULT hr)
 {
--- debug_helpers.h
+++ debug_helpers.h
@@ -3,6 +3,26 @@
 /**
  * @file debug_helpers.h
  * @author T.Ostaszewski
+ * @par License
+ * @code Copyright 2012 Tomasz Ostaszewski. All rights reserved.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * 	1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *	2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation 
+ * 	and/or other materials provided with the distribution.
+  * THIS SOFTWARE IS PROVIDED BY Tomasz Ostaszewski AS IS AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ * IN NO EVENT SHALL Tomasz Ostaszewski OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+ * SUCH DAMAGE.
+  * The views and conclusions contained in the software and documentation are those of the 
+ * authors and should not be interpreted as representing official policies, 
+ * either expressed or implied, of Tomasz Ostaszewski.
+ * @endcode
  * @date 04-Jan-2012
  * @brief
  * @details
@@ -22,7 +42,6 @@ extern "C" {
  * from Sysinternal.
  * @param[in] formatString
  * @return
- * @author T.Ostaszewski
  */
 HRESULT debug_output(LPCTSTR formatString, ...);
 
@@ -33,15 +52,6 @@ HRESULT debug_output(LPCTSTR formatString, ...);
  * from Sysinternal.
  * @param[in] formatString
  * @return
- * @author T.Ostaszewski
- */
-HRESULT debug_outputln(LPCTSTR formatString, ...);
-
-/*!
- * @brief Displays a message box with a textual code for HRESULT given
- * @param[in] wLine line number to be displayed in the message box
- * @param[in] lpszFile file name to be displayed in the message box
- * @author T.Ostaszewski
  */
 void ErrorHandlerEx(WORD wLine, LPSTR lpszFile);
 
