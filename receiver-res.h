7a2fd1991dc7c279912ff48600c18f90074b7a4f
Tomasz Ostaszewski <tomasz.ostaszewski@interia.pl>
2012-01-21 14:15:03 +0100

    Moved to separate resource files.

---
+++ receiver-res.h
@@ -0,0 +1,31 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDI_MAINICON                            101
+#define IDI_DISK                                104
+#define IDD_MAIN_RECEIVER                       116
+#define IDR_0_1                                 118
+#define IDD_RECEIVER_SETTINGS                   125
+#define IDR_MENU_RECEIVER                       129
+#define IDC_RECEIVER_BUFFER_FILL                1000
+#define IDC_BUFFER_BYTES_EDIT                   1001
+#define IDC_MCAST_SETTINGS                      1002
+#define IDC_MMTIMER_EDIT_CTRL                   1003
+#define IDC_MMTIMER_SPIN                        1004
+#define IDC_POLL_SLEEP_TIME_EDIT                1006
+#define IDC_PLAY_BUFFER_SIZE_EDIT               1007
+#define ID_RECEIVER_SETTINGS                    1007
+#define IDC_PLAY_BUFFER_SIZE_SPIN               1008
+#define IDS_APPTITLE_SENDER                     40000
+#define ID_RECEIVER_STARTRCV                    40000
+#define IDS_APPTITLE_RECEIVER                   40001
+#define ID_RECEIVER_STOPRCV                     40001
+#define IDM_ABOUT_RECEIVER                      40002
+#define IDT_TIMER_1                             40002
+#define IDS_APPTITLE                            40003
+#define ID_RECEIVER_JOINMCAST                   40011
+#define ID_RECEIVER_LEAVEMCAST                  40012
+#define ID_RECEIVER_PLAY                        40015
+#define ID_RECEIVER_STOP                        40016
+#define IDC_POLL_SLEEP_TIME_SPIN                40020
