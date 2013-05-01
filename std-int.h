3984b4ccc55e3be885883d72afa06cda86b88385
Tomasz Ostaszewski <tomasz.ostaszewski@interia.pl>
2013-05-01 17:11:35 +0200

    Added installer

--- std-int.h
+++ std-int.h
@@ -44,13 +44,15 @@
 #if !defined STD_INT_H_809467C6_E3E1_479D_95E9_98B4CB49D026
 #define STD_INT_H_809467C6_E3E1_479D_95E9_98B4CB49D026
 
-#if defined _MSC_VER && _MSC_VER < 1600
+#if defined _MSC_VER && _MSC_VER < 1500
 /* VC2003 does not have <stdint.h>.
  * Use the one provided by Mike Acton 
  */
 #include "vc2003stdint.h"
-#else
+#elif defined _MSC_VER && _MSC_VER < 1600
 /* VC2010 has its own <stdint.h> */
+#include "vc2009stdint.h"
+#else
 #include <stdint.h>
 #endif
 
